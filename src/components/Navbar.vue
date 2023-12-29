d3c18510a92d643c2e0a1ef0d42723c442687645
moath <dev2@slant3d.com>
2023-12-29 13:57:23 -0700

    ss

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,56 +1,21 @@
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-black px-3" >
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+      <div class="d-flex flex-column align-items-center p-2">
+        <img alt="logo" src="../assets/img/logo.png" height="45" />
       </div>
     </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
-        </li>
-      </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <div>
-        <button class="btn text-light" @click="toggleTheme"><i class="mdi" :class="theme == 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"></i></button>
-      </div>
-      <Login />
-    </div>
   </nav>
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
-import { loadState, saveState } from '../utils/Store.js';
-import Login from './Login.vue';
-export default {
-  setup() {
 
-    const theme = ref(loadState('theme') || 'light')
-
-    onMounted(() => {
-      document.documentElement.setAttribute('data-bs-theme', theme.value)
-    })
-
-    return {
-      theme,
-      toggleTheme() {
-        theme.value = theme.value == 'light' ? 'dark' : 'light'
-        document.documentElement.setAttribute('data-bs-theme', theme.value)
-        saveState('theme', theme.value)
-      }
-    }
-  },
-  components: { Login }
-}
 </script>
+
+
+
+
+
 
 <style scoped>
 a:hover {
@@ -62,11 +27,17 @@ a:hover {
 }
 
 .navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
+  border-bottom: 2px solid var(--bs-white);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
-
+.btn-power {
+  background-color: #f00; /* Red for power off, green for on */
+  color: #fff;
+}
+.btn-power.on {
+  background-color: #0f0; /* Green when active */
+}
 @media screen and (min-width: 768px) {
   nav {
     height: 64px;
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container">
+    <div class="content-box">
+      <h2 class="title">We are doing some fixes! Be back soon.</h2>
+      <p class="message">
+        Hello,
+        <br>
+        <br>
+        We are very sorry for the trouble, but a problem has come up with the Etsy app. We are temporarily pausing operations to fix the issue and intend to resume shortly.
+        <br>
+        <br>
+        We are fulfilling all orders currently sent through to us. However, the integrity of our service is of utmost importance to us.
+        <br>
+        <br>
+        We have written a blog post that goes into more detail on the Slant 3D Website. If you are in need of assistance, please email <a href="mailto:info@slant3d.com">info@slant3d.com</a>.
+        <br>
+        <br>
+        Thank you so much for your patience.
+        <br>
+        <br>
+        - The Team at Slant 3D
+      </p>
     </div>
   </div>
 </template>
@@ -14,30 +28,48 @@
 export default {
   setup() {
     return {
-      
     }
   }
 }
 </script>
 
-<style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f4f4f4;
+  font-family: 'Arial', sans-serif;
+}
 
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
+.content-box {
+  max-width: 600px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  /* text-align: center; */
+}
 
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.title {
+  margin-bottom: 20px;
+  font-size: 24px;
+  color: #333;
+}
+
+.message {
+  font-size: 16px;
+  line-height: 1.6;
+  color: #555;
+}
+
+a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
 }
 </style>
